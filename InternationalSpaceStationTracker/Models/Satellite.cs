acbf953d4421d59541ae28be9299e316974751e9
David Greenland <David.Greenland@brighthr.com>
2024-11-05 16:23:09 +0000

    YLW-3251: Adds GetSatellites

---
+++ InternationalSpaceStationTracker/Models/Satellite.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InternationalSpaceStationTracker.Models;
+
+public class Satellite
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, ID: {Id}";
+    }
+}
---
+++ InternationalSpaceStationTracker/Services/SatelliteService.cs
@@ -0,0 +1,38 @@
+﻿using InternationalSpaceStationTracker.Models;
+using System.Text.Json;
+using System.Web;
+
+namespace InternationalSpaceStationTracker.Services;
+
+public class SatelliteService
+{
+    private static readonly HttpClient Client = new()
+    {
+        BaseAddress = new Uri("https://api.wheretheiss.at/v1/")
+    };
+
+public async Task<IEnumerable<Satellite>> GetSatellites()
+    {
+        var response = await Client.GetAsync("satellites");
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<IEnumerable<Satellite>>(result) ?? Enumerable.Empty<Satellite>();
+    }
+
+    public async Task<SatelliteDetail> GetSingleSatellite(int id)
+    {
+        //var path = HttpUtility.ParseQueryString($"https://api.wheretheiss.at/v1/satellites/{id}");
+        //path["units"] = "miles";
+        //Console.WriteLine(path.ToString());
+        var response = await Client.GetAsync($"satellites/{id}&units=miles");
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        var a = JsonSerializer.Deserialize<SatelliteDetail>(result);
+
+        return a;
+    }
+
+    public async Task<Location>
+}
